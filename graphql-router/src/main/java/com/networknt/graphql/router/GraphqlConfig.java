dc27f32e7fea591b2b4c21cefe07bb6d69916027
Steve Hu <stevehu@gmail.com>
2017-03-24 17:53:10 -0400

    fixes #2 add graphql.yml config for path and enable graphiql

---
+++ graphql-router/src/main/java/com/networknt/graphql/router/GraphqlConfig.java
@@ -0,0 +1,28 @@
+package com.networknt.graphql.router;
+
+/**
+ * Created by steve on 24/03/17.
+ */
+public class GraphqlConfig {
+    String path;
+    boolean enableGraphiQL;
+
+    public GraphqlConfig() {
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    public boolean isEnableGraphiQL() {
+        return enableGraphiQL;
+    }
+
+    public void setEnableGraphiQL(boolean enableGraphiQL) {
+        this.enableGraphiQL = enableGraphiQL;
+    }
+}
--- graphql-router/src/main/java/com/networknt/graphql/router/GraphqlRouter.java
+++ graphql-router/src/main/java/com/networknt/graphql/router/GraphqlRouter.java
@@ -1,5 +1,6 @@
 package com.networknt.graphql.router;
 
+import com.networknt.config.Config;
 import com.networknt.info.ServerInfoGetHandler;
 import com.networknt.server.HandlerProvider;
 import io.undertow.Handlers;
@@ -10,13 +11,15 @@ import io.undertow.util.Methods;
  * Created by stevehu on 2017-03-22.
  */
 public class GraphqlRouter implements HandlerProvider {
+    public static final String CONFIG_NAME = "graphql";
+    static GraphqlConfig config = (GraphqlConfig) Config.getInstance().getJsonObjectConfig(CONFIG_NAME, GraphqlConfig.class);
+
     @Override
     public HttpHandler getHandler() {
         return Handlers.routing()
-                .add(Methods.GET, "/graphql", new GraphqlGetHandler())
-                .add(Methods.POST, "/graphql", new GraphqlPostHandler())
+                .add(Methods.GET, config.getPath(), new GraphqlGetHandler())
+                .add(Methods.POST, config.getPath(), new GraphqlPostHandler())
                 .add(Methods.GET, "/server/info", new ServerInfoGetHandler())
                 ;
     }
-
 }
