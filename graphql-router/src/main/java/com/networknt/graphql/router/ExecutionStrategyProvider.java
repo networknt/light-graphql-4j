a4a88653f3c564555c41574231bd512fca3f7371
Steve Hu <stevehu@gmail.com>
2018-10-03 16:17:59 -0400

    fixes #51 remove security.yml and add graphql-validator.yml

--- graphql-router/src/main/java/com/networknt/graphql/router/ExecutionStrategyProvider.java
+++ graphql-router/src/main/java/com/networknt/graphql/router/ExecutionStrategyProvider.java
@@ -13,16 +13,19 @@ public interface ExecutionStrategyProvider {
 
     /**
      * Return an execution strategy to use for queries or null to use the default.
+     * @return ExecutionStrategy a query execution strategy
      */
     ExecutionStrategy getQueryExecutionStrategy();
 
     /**
      * Return an execution strategy to use for mutations or null to use the default.
+     * @return ExecutionStrategy a mutation execution strategy
      */
     ExecutionStrategy getMutationExecutionStrategy();
 
     /**
      * Return an execution strategy to use for subscriptions or null to use the default.
+     * @return ExecutionStrategy a subscription execution strategy
      */
     ExecutionStrategy getSubscriptionExecutionStrategy();
 }
