8a7b5ee9b13b49273327c773c7308afe5f588c95
Steve Hu <stevehu@gmail.com>
2019-06-11 11:06:54 -0400

    fixes #56 migrate to Java 11 in jdk11 branch

--- graphql-security/src/main/java/com/networknt/graphql/security/JwtVerifyHandler.java
+++ graphql-security/src/main/java/com/networknt/graphql/security/JwtVerifyHandler.java
@@ -18,12 +18,12 @@ package com.networknt.graphql.security;
 
 import com.networknt.audit.AuditHandler;
 import com.networknt.config.Config;
-import com.networknt.exception.ExpiredTokenException;
 import com.networknt.graphql.common.GraphqlUtil;
 import com.networknt.handler.Handler;
 import com.networknt.handler.MiddlewareHandler;
 import com.networknt.httpstring.HttpStringConstants;
 import com.networknt.security.JwtHelper;
+import com.networknt.status.exception.ExpiredTokenException;
 import com.networknt.utility.Constants;
 import com.networknt.utility.ModuleRegistry;
 import io.undertow.Handlers;
